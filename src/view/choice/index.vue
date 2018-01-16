4e1c28144024d692c05920125596f2ecd2c72d76
KingsLighter <rzliang1991@gmail.com>
2018-01-16 15:40:15 +0800

    第一次提交，增加choice模块路径

---
+++ src/view/choice/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="part">
+        <div class="left-part">
+            <div class="banner">
+                <i></i>
+                <p>网游</p>
+                <p>查看更多</p>
+                <div></div>
+            </div>
+            <ul>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <p>阿超最强女主播开团</p>
+                    </div>
+                </li>
+                <div></div>
+            </ul>
+        </div>
+        <div class="right-part">
+            <div>
+                <p>游戏下载排行</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less" src="./style.less"></style>
+<script src="./script.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
