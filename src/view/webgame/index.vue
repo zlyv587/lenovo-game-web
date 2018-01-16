06aa8241de6d0428e20faa1ed6b4cffb578325f7
CedarLiu <lxs19951201@163.com>
2018-01-16 18:26:13 +0800

    新增网游界面

---
+++ src/view/webgame/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="webgame" >
+    	<div class="tab">这是一个导航</div>
+        <div class="top">
+        	<img src="../../assets/top.png" alt="" />
+        	<div class="download">
+        		<h3>贪玩蓝月</h3>
+        		<p>一刀九九九，不删档内测</p>
+        		<p>火爆开启</p>
+        		<div class="download-btn">立即下载<span class="icon-xiazai"></span><span class="bulk">2GB</span></div>
+        	</div>
+        </div>
+        <div class="banner">这是一轮播</div>
+        <div class="recommendList">
+        	<div class="line"></div>
+        	<div class="title">热门推荐</div>
+        	
+        	<p class="more">查看更多</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less" src="./style.less"></style>
+<script src="./script.js"></script> 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
