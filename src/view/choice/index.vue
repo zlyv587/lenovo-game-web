7f2589f2abe4244d79144f05a32ae7d5d3a69eb5
KingsLighter <rzliang1991@gmail.com>
2018-01-16 18:13:48 +0800

    第二次提交，修改了common.less的app宽度

--- src/view/choice/index.vue
+++ src/view/choice/index.vue
@@ -44,7 +44,66 @@
         <div class="right-part">
             <div>
                 <p>游戏下载排行</p>
+                <i class="border"></i>
             </div>
+            <ul>
+                <li>
+                    <i>1</i>
+                    <div></div>
+                    <p>热血传奇</p>
+                    <span>立即下载</span>
+                    <p class="none">90万人在玩</p>
+                </li>
+                <li>
+                    <i>2</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>3</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>4</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>5</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>6</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>7</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+                <li>
+                    <i>8</i>
+                    <div class="none"></div>
+                    <p>热血传奇</p>
+                    <span class="none">立即下载</span>
+                    <p>90万人在玩</p>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
