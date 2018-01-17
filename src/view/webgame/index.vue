8faeff6ec67f3ed8e18d9b10ae70611a568af20b
CedarLiu <lxs19951201@163.com>
2018-01-17 18:46:26 +0800

    修改样式

--- src/view/webgame/index.vue
+++ src/view/webgame/index.vue
@@ -1,21 +1,46 @@
 <template>
     <div class="webgame" >
-    	<div class="tab">这是一个导航</div>
-        <div class="top">
+    	<div class="tab">导航</div>
+        <div class="top-banner">
         	<img src="../../assets/top.png" alt="" />
         	<div class="download">
         		<h3>贪玩蓝月</h3>
         		<p>一刀九九九，不删档内测</p>
         		<p>火爆开启</p>
-        		<div class="download-btn">立即下载<span class="icon-xiazai"></span><span class="bulk">2GB</span></div>
+        		<div class="download-btn">立即下载<span class="icon-mokuaiguanli"></span><span class="bulk">2GB</span></div>
         	</div>
         </div>
-        <div class="banner">这是一轮播</div>
-        <div class="recommendList">
-        	<div class="line"></div>
-        	<div class="title">热门推荐</div>
-        	
-        	<p class="more">查看更多</p>
+        <div class="mid-banner">
+        	<div class="item">轮播</div>
+        	<div class="item">轮播</div>
+        	<div class="item">轮播</div>
+        	<div class="item">轮播</div>
+        </div>
+        <div class="recommend">
+        	<div class="title">
+        		<div class="line"></div>
+        		<h3>热门推荐</h3>
+        		<div class="filler"></div>
+        		<p>查看更多</p>
+        	</div>
+        	<div class="list">
+	        	<div class="item">
+	        		<img src="../../assets/rec1.png" alt="" />
+	        		<div class="intro">
+	        			<h3>贪玩蓝月</h3>
+	        			<p>一刀999一刀999一刀999一刀999一刀999一刀999一刀999</p>
+	        			<span><span class="">1</span>下载次数<span>999999999</span></span>
+	        		</div>
+	        	</div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        	<div class="item"></div>
+	        </div>
         </div>
     </div>
 </template>
