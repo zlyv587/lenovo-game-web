e4ccf1bebab26352853be8ef3024a2863c1f21e8
luchao <luchao2060@163.com>
2018-01-19 11:18:28 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -7,8 +7,9 @@ import 'babel-polyfill';
 import {init} from './common/request.js';
 
 import './main.less';
-
-import 'swiper/dist/css/swiper.min.css'
+// 引入头部Header，公共组件
+import Header from './components/header';
+Vue.use(Header);
 
 
 Vue.prototype.$bus = new Vue();
---
+++ src/view/header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="head_top">
+      <ul class="head_ul">
+          <li>精选</li>
+          <li>网游</li>
+          <li>页游</li>
+          <li>手游</li>
+      </ul>
+  </div>
+</template>
+
+<style scoped lang="less" src="./style.less"></style>
+<script src="./script.js"></script>
