8a5e8740817036c485501c1c6286515c935e8923
CedarLiu <lxs19951201@163.com>
2018-01-22 11:24:01 +0800

    修改文件名以及增加推荐页

---
+++ src/view/recommend/index.vue
@@ -0,0 +1,175 @@
+<template>
+	<div class="recommend">
+		<div class="back"><返回</div>
+		<ul class="family">
+			<li><a href="javascript:void">全部(1000)</a></li>
+			<li><a href="javascript:void">一直全部(1000)</a></li>
+			<li><a href="javascript:void">策略全部(1000)</a></li>
+			<li><a href="javascript:void">回合全部(1000)</a></li>
+			<li><a href="javascript:void">吃鸡全部(1000)</a></li>
+			<li><a href="javascript:void">回合制(1000)</a></li>
+			<li><a href="javascript:void">赛车游戏(1000)</a></li>
+			<li><a href="javascript:void">角色扮演(1000)</a></li>
+			<li><a href="javascript:void">回合制(1000)</a></li>
+			<li><a href="javascript:void">赛车游戏(1000)</a></li>
+			<li><a href="javascript:void">角色扮演(1000)</a></li>
+		</ul>
+		<div class="sortby">
+			<div class="btn date">更新时间↓</div>
+			<div class="btn times">下载次数↓</div>
+			<div class="btn rating">综合评分↓</div>
+		</div>
+		<div class="games">
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="item">
+				<img class="img" src="../../assets/bg_img1.png" alt="...">
+				<div class="msg">
+					<h3>贪玩蓝月</h3>
+					<span>网游RPG</span>
+					<span>114Mb</span>
+					<span>2018-01-01</span>
+				</div>
+			</div>
+			<div class="pages">12</div>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="less" src="./style.less"></style>
+<script src="./script.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
