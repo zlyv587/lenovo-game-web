47d6b99969ea70f92a4353206b12388d0de21892
KingsLighter <rzliang1991@gmail.com>
2018-01-23 10:32:35 +0800

    2018-1-23修改

--- src/view/choice/index.vue
+++ src/view/choice/index.vue
@@ -2,7 +2,7 @@
     <div id="appChoice">
         <v-header></v-header>
         <v-swipe :style="styleObject"></v-swipe>
-        <v-weGame></v-weGame>
+        <!--<v-weGame></v-weGame>-->
         <v-weGame></v-weGame>
         <v-moGame></v-moGame>
     </div>
